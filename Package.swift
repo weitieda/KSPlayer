2c8a1c27db4cdc2ade986757bbca6887003495b9
kintan <kingslay@icloud.com>
2023-08-05 10:31:10 +0800

    test

--- Package.swift
+++ Package.swift
@@ -33,12 +33,15 @@ let package = Package(
 var ffmpegKitPath = FileManager.default.currentDirectoryPath + "/FFmpegKit"
 if !FileManager.default.fileExists(atPath: ffmpegKitPath), let url = URL(string: #file) {
     let path = url.deletingLastPathComponent().path
-    ffmpegKitPath = path + "/FFmpegKit"
+    // 解决用xcode引入spm的时候，依赖关系出错的问题
+    if !path.hasSuffix("checkouts") {
+        ffmpegKitPath = path + "/FFmpegKit"
+    }
 }
 
-if FileManager.default.fileExists(atPath: ffmpegKitPath + "/Package.swift"), !ffmpegKitPath.contains("checkouts") {
+if FileManager.default.fileExists(atPath: ffmpegKitPath + "/Package.swift") {
     package.dependencies += [
-        .package(path: ffmpegKitPath)
+        .package(path: ffmpegKitPath),
     ]
 } else {
     package.dependencies += [
